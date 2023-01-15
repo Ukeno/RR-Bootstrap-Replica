c03c75d8c93682213a1af096236fc4b3625133cf
Kenny K <koablood@gmail.com>
2023-01-14 22:31:59 -0800

    Instacart-Bootstrap-Remake

---
+++ src/components/TopBar.js
@@ -0,0 +1,22 @@
+// import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+export default function TopBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg" >
+                <Navbar.Brand style={{ margin: "5px 0 0 30px", padding: "9px 0 0 9px" }} href="#home"><img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" alt="Instacart" /> </Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Nav style={{ padding: "24px 40px 20px 840px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex" }} >
+                        <Nav.Link style={{ color: "#343538" }} href="#LogIn">Log In</Nav.Link>&emsp;&emsp;
+                        <Button variant="success" style={{ fontSize: "18px" }}>Sign Up</Button>{' '}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
